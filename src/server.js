5140a3b857be30c9d7a6043977aacdfd91a231d4
teachme10code <thinkfulchris@gmail.com>
2017-02-20 16:39:36 -0800

    changed port back to 8080

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use('/posts', blogPostRouter);
 let server;
 
 function runServer() {
-  const port = process.env.PORT || 8081;
+  const port = process.env.PORT || 8080;
   return new Promise((resolve, reject) => {
     server = app.listen(port, () => {
       console.log(`Your app is listening on port ${port}`);
